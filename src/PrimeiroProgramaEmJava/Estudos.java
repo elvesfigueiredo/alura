fbcd1651c4158f5d4a4173ac9e7b8c336c0ee9df
elvescomsono <126408967+elvesfigueiredo@users.noreply.github.com>
2025-12-11 19:35:08 -0300

    More exercices

---
+++ src/PrimeiroProgramaEmJava/Estudos.java
@@ -0,0 +1,8 @@
+package PrimeiroProgramaEmJava;
+// Crie uma classe chamada Estudos para imprimir anotações de estudos no console.
+public class Estudos {
+    public static void main(String[] args) {
+        System.out.println("JVM (Java Virtual Machine) - é a Máquina Virtual do Java responsável por executar o bytecode");
+        System.out.println("JRE (Java Runtime Environmet) - Ambiente de Execução do Java que fornece as bibliotecas padrões do Java");
+    }
+}
---
+++ src/PrimeiroProgramaEmJava/Perfil.java
@@ -0,0 +1,13 @@
+package PrimeiroProgramaEmJava;
+/*
+Crie um projeto Java no IntelliJ que deve conter uma classe chamada Perfil, com o método main,
+que ao executar deverá imprimir um cumprimento personalizado no console. Por exemplo, "Olá, [Seu Nome]!".
+Modifique o programa para imprimir uma segunda mensagem de cumprimento personalizada. Por exemplo, "Tudo bem?".
+ */
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá Alura!");
+        System.out.println("Tudo bem?");
+    }
+}
+
